f851ce4b28ac3f4ec58de35a870c2bd35be768cc
Rahul Das <109250596+Rahul-Das14@users.noreply.github.com>
2024-12-18 23:24:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Consecutive Subsequence/longest-consecutive-subsequence.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            // Read first array
+            String input = br.readLine();
+            String[] inputArray = input.split(" ");
+            int[] arr = Arrays.stream(inputArray).mapToInt(Integer::parseInt).toArray();
+
+            Solution ob = new Solution();
+            int res = ob.findLongestConseqSubseq(arr);
+
+            System.out.println(res);
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+
+    // Function to return length of longest subsequence of consecutive integers.
+    public int findLongestConseqSubseq(int[] arr) 
+    {
+        int n = arr.length;
+        if (n == 0)
+            return 0;
+
+        int longest = 1;
+        Set<Integer> set = new HashSet<>();
+
+        // put all the array elements into set
+        for (int i = 0; i < n; i++) {
+            set.add(arr[i]);
+        }
+
+        // Find the longest sequence
+        for (int it : set) {
+            // if 'it' is a starting number
+            if (!set.contains(it - 1)) {
+                // find consecutive numbers
+                int cnt = 1;
+                int x = it;
+                while (set.contains(x + 1)) {
+                    x = x + 1;
+                    cnt = cnt + 1;
+                }
+                longest = Math.max(longest, cnt);
+            }
+        }
+        return longest;
+    }
+}
+
